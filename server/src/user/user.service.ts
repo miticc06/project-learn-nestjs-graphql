caacc1cffca75a2ff1d3c3c0c8a92e8e8334a79c
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-25 10:09:54 +0700

    check userinput signup

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -4,7 +4,7 @@ import { User } from './user.entity';
 import { MongoRepository } from 'typeorm';
 import { UserInput } from './user.input';
 import * as uuid from 'uuid'
-
+import { UserInputError } from 'apollo-server-express'
 @Injectable()
 export class UserService {
     constructor(
@@ -13,15 +13,32 @@ export class UserService {
     ) { }
 
     async createUser(input: UserInput): Promise<User> {
+
+        if (!input.username || input.username.trim().length === 0) {
+            throw new UserInputError('Username khong duoc de trong!');
+        }
+
+        if (!input.password || input.password.length === 0) {
+            throw new UserInputError('Password khong duoc de trong!');
+        }
+
+        const existingUser = await this.userRepository.findOne({ username: input.username.trim() });
+        console.log(existingUser);
+        if (existingUser) {
+            throw new Error('Username da ton tai!');
+        }
+
         const user = new User();
         user._id = uuid.v4();
-        user.username = input.username;
+        user.username = input.username.trim();
         user.password = input.password;
-        return await this.userRepository.save(user);
+
+        return this.userRepository.save(user);
     }
 
     async findAll(): Promise<User[]> {
         return await this.userRepository.find();
-    }
+    } 
+
 
 }
