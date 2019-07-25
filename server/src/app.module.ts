5939f43293ea578e7b6a08f4d74d23799f706498
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-25 09:14:56 +0700

    add mongodb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from "@nestjs/graphql";
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      playground: true
+    }),
+    TypeOrmModule.forRoot({
+      url: 'mongodb://localhost/project_learn_nestjs_graphql',
+      type: 'mongodb',
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      useNewUrlParser: true,
+      logging: true
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ server/src/user/user.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver, Query } from '@nestjs/graphql';
+@Resolver('User')
+export class UserResolver {
+    @Query(() => String)
+    async hello() {
+        return 'world';
+    }
+}
